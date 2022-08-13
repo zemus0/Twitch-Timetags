06d44277909fa55c9a9a059f754b511790a91e88
zemaus <62078528+zemaus@users.noreply.github.com>
2022-08-13 00:40:00 -0400

    spawn new window instead of injecting html to twitch

--- background.js
+++ background.js
@@ -1,6 +1,12 @@
+var run_once = false;
 chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        files: ['insert.js']
-    });
+    if(!run_once) {
+        run_once = true;
+        chrome.windows.create({
+            url: "chat.html",
+            type: "popup",
+            height:500,
+            width:300,
+        });
+    }
 });
