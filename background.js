1731a8c16f043f64790bb42a87b92f8050ad0c08
zemaus <62078528+zemaus@users.noreply.github.com>
2022-08-18 12:02:25 -0400

    remove run only once

--- background.js
+++ background.js
@@ -1,20 +1,17 @@
-var run_once = false;
 chrome.action.onClicked.addListener((tab) => {
-    if(!run_once) {
-        run_once = true;
-        chrome.storage.local.set({"key": tab.id});
+    chrome.storage.local.set({"key": tab.id});
 
-        chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            files:["inject.js"]
-        });
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files:["inject.js"]
+    });
 
-        chrome.windows.create({
-            url: "timestamp.html",
-            type: "popup",
-            height:500,
-            width:400,
-        });
-    }
+    chrome.windows.create({
+        url: "timestamp.html",
+        type: "popup",
+        height:500,
+        width:400,
+    });
+
 });
 
