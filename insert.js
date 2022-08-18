7044a0dcee7381d3ca5150bca359505a19ef6ffd
zemaus <62078528+zemaus@users.noreply.github.com>
2022-08-18 12:03:59 -0400

    renamed

--- insert.js
+++
@@ -1,3 +0,0 @@
-document.getElementsByTagName("head")[0].appendChild('<link rel="stylesheet" href="twitch_timestamp.css">')
-var chat = document.getElementsByClassName("Layout-sc-nxg1ff-0 cHunhC")[0];
-chat.insertAdjacentHTML('beforeend','<div style="height:50%"></div>');
